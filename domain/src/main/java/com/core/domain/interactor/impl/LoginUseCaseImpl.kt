7098d28aa43df0c2e9f18f57bfcaa2d1c5bd2ff8
Ángel Gómez <raxden.dev@gmail.com>
2019-01-31 17:58:40 +0100

    update

--- domain/src/main/java/com/core/domain/interactor/impl/LoginUseCaseImpl.kt
+++ domain/src/main/java/com/core/domain/interactor/impl/LoginUseCaseImpl.kt
@@ -1,17 +1,18 @@
 package com.core.domain.interactor.impl
 
 import com.core.domain.Project
+import com.core.domain.User
 import com.core.domain.interactor.GetProjectDetailUseCase
+import com.core.domain.interactor.LoginUseCase
 import com.core.domain.repository.ProjectRepository
+import com.core.domain.repository.UserRepository
 import io.reactivex.Single
 import javax.inject.Inject
 
-class GetProjectDetailUseCaseImpl @Inject constructor(
-    projectRepository: ProjectRepository
-) : BaseUseCaseImpl<ProjectRepository>(projectRepository),
-    GetProjectDetailUseCase {
+class LoginUseCaseImpl @Inject constructor(
+        userRepository: UserRepository
+) : BaseUseCaseImpl<UserRepository>(userRepository),
+        LoginUseCase {
 
-    override fun execute(userId: String, projectName: String): Single<Project> {
-        return repository.detail(userId, projectName)
-    }
+    override fun execute(username: String, password: String): Single<User> = repository.login(username, password)
 }
