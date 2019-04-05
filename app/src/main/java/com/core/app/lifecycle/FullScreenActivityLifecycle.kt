17f605513472e9bbaae69c57d4d3c2021830dbcd
Ángel Gómez <raxden.dev@gmail.com>
2019-04-05 12:35:45 +0200

    update

--- app/src/main/java/com/core/app/lifecycle/FullScreenActivityLifecycle.kt
+++ app/src/main/java/com/core/app/lifecycle/FullScreenActivityLifecycle.kt
@@ -1,5 +1,6 @@
 package com.core.app.lifecycle
 
+import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
@@ -18,9 +19,8 @@ class FullScreenActivityLifecycle @Inject internal constructor(
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     private fun onCreate() {
-        mActivity.window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        mActivity.window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
     }
 }
