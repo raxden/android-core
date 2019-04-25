32813f72063db31d90f92ccf0e90e76d132ec11b
Ángel Gómez <raxden.dev@gmail.com>
2019-04-25 17:03:35 +0200

    update

--- data/src/main/java/com/core/data/network/entity/mapper/UserEntityDataMapper.kt
+++ data/src/main/java/com/core/data/network/entity/mapper/UserEntityDataMapper.kt
@@ -4,14 +4,18 @@ import android.content.Context
 import com.core.commons.DataMapper
 import com.core.commons.extension.mapTo
 import com.core.data.network.entity.ProjectEntity
+import com.core.data.network.entity.UserEntity
 import com.core.domain.Project
+import com.core.domain.User
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class ProjectEntityDataMapper @Inject internal constructor(context: Context) : DataMapper<ProjectEntity, Project>(context) {
+class UserEntityDataMapper @Inject internal constructor(context: Context) : DataMapper<UserEntity, User>(context) {
 
-    override fun transform(source: ProjectEntity): Project = source.mapTo()
+    override fun transform(source: UserEntity): User = source.mapTo<User>().also {
+        it.username = source.login ?: ""
+    }
 
-    override fun inverse(source: Project): ProjectEntity = source.mapTo()
+    override fun inverse(source: User): UserEntity = source.mapTo()
 }
