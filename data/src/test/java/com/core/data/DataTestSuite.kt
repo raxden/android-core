b0e5865c28e294f85813eeb550a62902d08c6e5e
Ángel Gómez <raxden.dev@gmail.com>
2019-05-17 13:25:53 +0200

    remove unnecesary import

--- data/src/test/java/com/core/data/DataTestSuite.kt
+++ data/src/test/java/com/core/data/DataTestSuite.kt
@@ -2,7 +2,6 @@ package com.core.data
 
 import com.core.data.network.AppGatewayTest
 import com.core.data.persistence.AccountDaoTest
-import com.core.data.repository.AccountRepositoryTest
 
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
