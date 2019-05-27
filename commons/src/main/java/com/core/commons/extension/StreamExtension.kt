b57dacc7d2f907920b90a6dbcda40e6f273056d6
Ángel Gómez <raxden.dev@gmail.com>
2019-05-27 14:50:14 +0200

    remove unnecesary files

--- commons/src/main/java/com/core/commons/extension/StreamExtension.kt
+++ commons/src/main/java/com/core/commons/extension/StreamExtension.kt
@@ -1,8 +1,36 @@
 package com.core.commons.extension
 
-import android.view.View
-import java.io.InputStream
+import android.util.Xml
+import timber.log.Timber
+import java.io.*
 
 fun InputStream.closeSilently() {
-    
+    try {
+        close()
+    } catch (e: IOException) {
+        Timber.e(e)
+    }
+}
+
+fun ObjectInputStream.closeSilently() {
+    try {
+        close()
+    } catch (e: IOException) {
+        Timber.e(e)
+    }
+}
+
+fun InputStream.readContent(encoding: Xml.Encoding = Xml.Encoding.UTF_8): String {
+    val sb = StringBuilder()
+    try {
+        val reader = BufferedReader(InputStreamReader(this, encoding.toString()))
+        for (line in reader.readLine()) sb.append(line).append("\n")
+    } catch (e: UnsupportedEncodingException) {
+        Timber.e(e)
+    } catch (e: IOException) {
+        Timber.e(e)
+    } finally {
+        closeSilently()
+    }
+    return sb.toString()
 }
