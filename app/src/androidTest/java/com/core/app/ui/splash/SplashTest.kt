59691a3346f41f01f5d696d9e823b0294d77b3a4
Ángel <raxden.dev@gmail.com>
2019-10-10 15:41:02 +0200

    working in instrumental test

--- app/src/androidTest/java/com/core/app/ui/splash/SplashTest.kt
+++ app/src/androidTest/java/com/core/app/ui/splash/SplashTest.kt
@@ -1,20 +1,19 @@
 package com.core.app.ui.splash
 
 import androidx.lifecycle.MutableLiveData
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import org.junit.runner.RunWith
-import com.core.app.R
 import com.core.app.ui.screens.splash.SplashActivity
 import com.core.app.ui.screens.splash.SplashViewModel
+import com.core.app.util.ViewModelUtil
+import com.core.domain.Forward
+import com.core.domain.User
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 
 @RunWith(AndroidJUnit4::class)
 class SplashTest {
@@ -23,22 +22,27 @@ class SplashTest {
     @JvmField
     val activityRule = ActivityTestRule(SplashActivity::class.java, true, true)
 
-    private lateinit var activity: SplashActivity
-    private lateinit var viewModel: SplashViewModel
+    private lateinit var splashActivity: SplashActivity
+    private lateinit var splashViewModel: SplashViewModel
 
-    private val versionData = MutableLiveData<String>()
+    private val versionLiveData = MutableLiveData<String>()
+    private val applicationReadyLiveData = MutableLiveData<Pair<Forward, User?>>()
 
     @Before
     fun init() {
-        viewModel = Mockito.mock(SplashViewModel::class.java)
-        Mockito.`when`(viewModel.version).thenReturn(versionData)
+        splashViewModel = Mockito.mock(SplashViewModel::class.java).also {
+            `when`(it.version).thenReturn(versionLiveData)
+            `when`(it.applicationReady).thenReturn(applicationReadyLiveData)
+        }
 
-        activity = activityRule.activity as SplashActivity
+        splashActivity = (activityRule.activity as SplashActivity).also {
+            it.viewModelFactory = ViewModelUtil.createFor(splashViewModel)
+        }
     }
 
     @Test
     fun anything() {
-        versionData.postValue("version 1.0")
-        onView(withId(R.id.version_name_view)).check(matches(ViewMatchers.isDisplayed()))
+//        versionData.postValue("version 1.0")
+//        onView(withId(R.id.version_name_view)).check(matches(ViewMatchers.isDisplayed()))
     }
 }
