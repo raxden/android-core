9a50a8564df9ce54b91244a42d8986a8d9f1a1da
Ángel Gómez <raxden.dev@gmail.com>
2019-10-24 15:54:47 +0200

    modularization [wip]

--- data/local/src/androidTest/java/com/core/data/local/dao/ProjectDaoTest.kt
+++ data/local/src/androidTest/java/com/core/data/local/dao/ProjectDaoTest.kt
@@ -2,40 +2,18 @@ package com.core.data.local.dao
 
 import com.core.data.local.BaseLocalTest
 import com.core.domain.Account
+import com.core.domain.Project
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Test
 
-class AccountDaoTest : BaseLocalTest() {
+class ProjectDaoTest : BaseLocalTest() {
 
-    private lateinit var accountDao: AccountDao
-
-    private val fakeAccount = Account("username")
+    private lateinit var projectDao: ProjectDao
 
     override fun setUp() {
         super.setUp()
 
-        accountDao = database.accountDao()
-
-        runBlocking {
-            accountDao.insert(fakeAccount)
-        }
-    }
-
-    @Test
-    fun retrieveUserFromDB() = runBlocking {
-        val account = database.accountDao().find(fakeAccount.username)
-        compareUsers(fakeAccount, account)
-    }
-
-    @Test
-    fun retrieveUserThatNotExistsInDB() = runBlocking {
-        val account = database.accountDao().find("username_not_exists")
-        assertNull(account)
-    }
-
-    private fun compareUsers(account: Account, accountToCompare: Account) {
-        assertEquals(account.username, accountToCompare.username)
     }
 }
